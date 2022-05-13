7e2b8c0bfff968cfcd8d896562676bd3c11d70d2
adam oates <adam@adamcodertrader.com>
2022-05-12 23:18:45 -0500

    clean up app.js

--- api/app.js
+++ api/app.js
@@ -38,12 +38,6 @@ app.use(
   })
 );
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "./views"));
-app.set("trust proxy", 1);
-
-app.use(express.static(path.join(__dirname, "./static")));
-
 app.use("/", routes({}));
 
 app.listen(port, () => {
